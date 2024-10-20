5f241b73cd059788d563827d8c344908ea6f3ac6
Jose Gutierrez <u202221518@upc.edu.pe>
2024-10-20 02:30:02 -0500

    feat(events): add eventsbounded context

---
+++ src/main/java/pe/upc/pawfectcarebackend/event/interfaces/rest/ClientController.java
@@ -0,0 +1,4 @@
+package pe.upc.pawfectcarebackend.event.interfaces.rest;
+
+public class ClientController {
+}
---
+++ src/test/java/pe/upc/pawfectcarebackend/PawFectCareBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.pawfectcarebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawFectCareBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
